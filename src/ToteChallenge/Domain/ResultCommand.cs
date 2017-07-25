bf5c9d9e49a507db4892b62526730e377587c16d
gsimoes <guilherme.simoes@williamhill.com.au>
2017-07-26 00:23:01 +1000

    implement get result command

--- src/ToteChallenge/Domain/ResultCommand.cs
+++ src/ToteChallenge/Domain/ResultCommand.cs
@@ -1,4 +1,6 @@
-﻿namespace ToteChallenge.Domain
+﻿using System;
+
+namespace ToteChallenge.Domain
 {
     public class ResultCommand : Command
     {
@@ -10,7 +12,11 @@
 
         public override void Execute(Tote context)
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine($"Win:{First}:${context.GetResult("w", First.ToString())}");
+            Console.WriteLine($"Place:{First}:${context.GetResult("p", First.ToString())}");
+            Console.WriteLine($"Place:{Second}:${context.GetResult("p", Second.ToString())}");
+            Console.WriteLine($"Place:{Third}:${context.GetResult("p", Third.ToString())}");
+            Console.WriteLine($"Exacta:{First},{Second}:${context.GetResult("e", $"{First},{Second}")}");
         }
     }
 }
