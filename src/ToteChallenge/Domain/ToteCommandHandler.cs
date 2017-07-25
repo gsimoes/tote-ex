7ddaba779a3550280ded7b2751e8a83db330b8f1
gsimoes <guilherme.simoes@williamhill.com.au>
2017-07-25 23:52:42 +1000

    adding command handler

---
+++ src/ToteChallenge/Domain/ToteCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace ToteChallenge.Domain
+{
+    public class ToteCommandHandler
+    {
+        private readonly ICommandParser _commandParser;
+        private readonly Tote _context;
+
+        public ToteCommandHandler(ICommandParser commandParser, Tote context)
+        {
+            _commandParser = commandParser;
+            _context = context;
+        }
+
+        public void Execute(string input)
+        {
+            var command = _commandParser.Parse(input);
+
+            command.Execute(_context);
+        }
+    }
+}
